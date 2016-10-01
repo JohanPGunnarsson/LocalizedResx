502d6b04ce872f6842ed8a9729ef67634f267789
Johan P Gunnarsson <johan@northware.com>
2016-10-01 13:56:47 +0200

    Initial version

---
+++ LocalizedResx/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Threading;
+using System.Windows;
+
+namespace LocalizedResx
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var newCulture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = newCulture;
+            Thread.CurrentThread.CurrentUICulture = newCulture;
+
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
